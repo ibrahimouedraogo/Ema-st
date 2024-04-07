4ba12ec56685971ba76f9c173e9360a0eb13244b
Ouedraogo ibrahim <ibrahimouedraogo@gmail.com>
2024-04-07 18:16:45 +0000

    First commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2024/5/24',function(event){
+     jQuery('#clock').countdown('2024/08/19',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> <b>:</b> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> <b>:</b> Hours</div> '
